b84148fc2770d65b8c989483aede599adf206f5e
Claudio Balbin <claudiobalbin@gmail.com>
2023-08-17 18:33:47 -0300

    enhancing the data cleaner

--- data_cleaner.py
+++ data_cleaner.py
@@ -4,9 +4,9 @@ import os
 import json
 
 # define a conexão com o banco de dados
-db = SqliteDatabase(":memory:")
-# db = PostgresqlDatabase('postgres', user='postgres', password='oCSM7dqa2nrR',
-#                            host='127.0.0.1', port=5432)
+# db = SqliteDatabase(":memory:")
+db = PostgresqlDatabase('postgres', user='postgres', password='oCSM7dqa2nrR',
+                           host='db', port=5432)
 
 class BaseModel(Model):
     """A base model that will use our Postgresql database"""
@@ -26,7 +26,8 @@ if 'log' not in db.get_tables():
     db.create_tables([Log])
 
 # Scanning for new logger outputs
-folder_path = 'logger_output'
+# folder_path = 'logger_output'
+folder_path = '/home/logger_output'
 target_extension = '.json'
 
 log_data = []
@@ -36,16 +37,21 @@ print("Looking for new files")
 for filename in os.listdir(folder_path):
     if filename.endswith(target_extension):
         file_path = os.path.join(folder_path, filename)
+        print(f"Handling file {file_path}")
         with open(file_path, 'r') as json_file:
-            json_data = json.load(json_file)
-            log = Log(
-                    download = json_data['download'] / 1000000,
-                    upload = json_data['upload'] / 1000000,
-                    ping = json_data['ping']
-                )
-            log.save()
-        try:
-            os.remove(file_path)
-            print(f"File '{file_path}' removed successfully.")
-        except OSError as e:
-            print(f"Error deleting '{file_path}': {e}")
+            try:
+                json_data = json.load(json_file)
+                log = Log(
+                        download = json_data['download'] / 1000000,
+                        upload = json_data['upload'] / 1000000,
+                        ping = json_data['ping']
+                    )
+                log.save()
+            except json.JSONDecodeError as e:
+                print(f"Error parsing JSON: {e}")
+            
+            try:
+                os.remove(file_path)
+                print(f"File '{file_path}' removed successfully.")
+            except OSError as e:
+                print(f"Error deleting '{file_path}': {e}")
